1919561e79991448843fc40bde2aaeeeb0ca58f9
Pratik <pratikrajaryan@gmail.com>
2021-08-10 15:40:36 +0530

    first commit

---
+++ src/Pages/PortfolioPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from '../Components/Card/Cards'
+
+function Portfolio() {
+    return (
+        <div className="portfoliopage">
+            <h1>My Projects</h1>
+      </div>
+    )
+}
+
+export default Portfolio
